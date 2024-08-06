ed76550d5275a2b26c034080ac5344172d6cd94c
Riya Jain <riya1jain567@gmail.com>
2024-08-06 20:46:00 +0530

    Add more styling

--- src/pages/CreatorPage/components/CreateSurveyForm.tsx
+++ src/pages/CreatorPage/components/CreateSurveyForm.tsx
@@ -45,53 +45,47 @@ export default function CreateSurveyForm() {
       <figure className="absolute-cover bg-gradient-to-r from-primary via-secondary to-secondary" />
       <figure className="absolute-cover bg-gradient-to-b from-transparent to-background" />
 
-      <div className="flex flex-col gap-y-6 z-10 relative w-1/3 rounded-xl p-6 bg-back">
-        <div className="">
-          <input
-            className="text-front w-full py-1 px-2 bg-transparent border-b border-front/20"
-            placeholder="Name of the Survey"
-          />
-        </div>
+      <div className="flex flex-col gap-y-6 relative w-1/2 rounded-xl p-6 bg-back">
+        <h2 className="text-3xl font-bold mb-4 bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text">
+          Create Survey Form
+        </h2>
 
-        <div className="">
-          <textarea
-            className="text-front w-full py-1 px-2 bg-transparent border-b border-front/20 focus-within:outline-none"
-            placeholder="Description for the Survey"
-            minLength={5}
-          />
-        </div>
-
-        <div className="">
-          <select
-            className="text-front w-full py-1 px-2 bg-transparent border-b border-front/20"
-            value={token ? token.token : ""}
-            onChange={(e) =>
-              settoken(
-                tokens.find((token) => token.token === e.target.value) || null
-              )
-            }
-          >
-            <option value="" disabled className="">
-              Select token
+        <input
+          className="text-front w-full py-1 px-2 bg-transparent border-b border-front/20"
+          placeholder="Name of the Survey"
+        />
+        <textarea
+          className="text-front w-full py-1 px-2 bg-transparent border-b border-front/20 focus-within:outline-none"
+          placeholder="Description for the Survey"
+          minLength={5}
+        />
+        <select
+          className="text-front w-full py-1 px-2 bg-transparent border-b border-front/20"
+          value={token ? token.token : ""}
+          onChange={(e) =>
+            settoken(
+              tokens.find((token) => token.token === e.target.value) || null
+            )
+          }
+        >
+          <option value="" disabled className="">
+            Select token
+          </option>
+          {tokens.map((token) => (
+            <option key={token.token} value={token.token}>
+              {token.name}
             </option>
-            {tokens.map((token) => (
-              <option key={token.token} value={token.token}>
-                {token.name}
-              </option>
-            ))}
-          </select>
-        </div>
+          ))}
+        </select>
 
-        <div className="">
-          <input
-            className="text-front w-full py-1 px-2 bg-transparent border-b border-front/20"
-            onChange={(e) => setAmount(Number(e.target.value))}
-            placeholder="Amount for the Survey"
-            type="number"
-          />
-        </div>
+        <input
+          className="text-front w-full py-1 px-2 bg-transparent border-b border-front/20"
+          onChange={(e) => setAmount(Number(e.target.value))}
+          placeholder="Amount for the Survey"
+          type="number"
+        />
 
-        <div className="">
+        <div>
           <input
             className="text-front w-full py-1 px-2 bg-transparent border-b border-front/20"
             placeholder="Number of Entries"
@@ -104,6 +98,9 @@ export default function CreateSurveyForm() {
             pool
           </div>
         </div>
+        <button className="w-max self-end px-4 py-2 text-front bg-gradient-to-r from-primary/40 to-secondary/40 rounded-md">
+          Create Form
+        </button>
       </div>
     </div>
   );
